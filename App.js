cd4a8ece428c54d932a17622bdd61cec7cb61f95
Sujin Joshi <>
2019-05-18 22:20:43 +0545

    logout button in drawer

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
 /* Example of SQLite Database in React Native */
 import React from "react";
-import { createStackNavigator, createAppContainer } from "react-navigation";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes
+} from "react-native-google-signin";
+import {
+  createStackNavigator,
+  createDrawerNavigator,
+  createAppContainer,
+  DrawerItems
+} from "react-navigation";
+import { StyleSheet, ScrollView, SafeAreaView, Text } from "react-native";
 import Home from "./pages/HomeScreen";
 import Menu from "./pages/MenuScreen";
 import Beverages from "./pages/beverages";
 import Foods from "./pages/foods";
 import Bakery from "./pages/bakery";
+import { Button } from "react-native-elements";
+
+_signOut = async () => {
+  //Remove user session from the device.
+  try {
+    await GoogleSignin.revokeAccess();
+    await GoogleSignin.signOut();
+    this.props.navigation.navigate("Home");
+    // this.setState({ user: null }); // Remove the user from your app's state as well
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const CustomDrawerContentComponent = props => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{ top: "always", horizontal: "never" }}
+    >
+      <Button onPress={this._signOut} title="Log out" />
+    </SafeAreaView>
+  </ScrollView>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
 
 const RootStack = createStackNavigator({
   HomeS: {
@@ -63,5 +104,22 @@ const RootStack = createStackNavigator({
   headerLayoutPreset: "center"
 });
 
-const App = createAppContainer(RootStack);
+const MainDrawerNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: RootStack
+    }
+  },
+  {
+    drawerOpenRoute: "DrawerOpen",
+    drawerCloseRoute: "DrawerClose",
+    drawerToggleRoute: "DrawerToggle",
+    contentComponent: CustomDrawerContentComponent,
+    navigationOptions: {
+      drawerLockMode: "locked-closed"
+    }
+  }
+);
+
+const App = createAppContainer(MainDrawerNavigator);
 export default App;
